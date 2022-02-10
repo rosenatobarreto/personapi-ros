b288c50257ecff05da2f236787239a91b6908d6b
rosenatobarreto <rosenatoblima@gmail.com>
2022-02-10 01:46:43 -0300

    Iniciando o projeto

---
+++ src/main/java/com/dio/personapiros/PersonapiRosApplication.java
@@ -0,0 +1,13 @@
+package com.dio.personapiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiRosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiRosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/personapiros/PersonapiRosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.personapiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiRosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
