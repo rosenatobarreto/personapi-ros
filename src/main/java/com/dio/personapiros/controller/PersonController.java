75d2f9599b1e1140549099521f58533ea7f3f62f
rosenatobarreto <rosenatoblima@gmail.com>
2022-02-11 00:31:31 -0300

    Developed person create operation

--- src/main/java/com/dio/personapiros/controller/PersonController.java
+++ src/main/java/com/dio/personapiros/controller/PersonController.java
@@ -1,28 +1,27 @@
 package com.dio.personapiros.controller;
 
 import com.dio.personapiros.dto.MessageResponseDTO;
-import com.dio.personapiros.repository.PersonRepository;
+import com.dio.personapiros.entity.Person;
+import com.dio.personapiros.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
-    public String createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        return personService.createPerson(person);
     }
 
 }
